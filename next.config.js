a10e55e3004db53f3417a87375219a3dbd062192
scottm-dev <105765424+scottm-dev@users.noreply.github.com>
2022-09-14 16:16:33 -0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+    /* config options here */
+    env: {
+        customKey: 'pickles-gm2',
+    },
+  }
+  
+  module.exports = nextConfig
